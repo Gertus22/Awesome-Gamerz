0c76927621dcaead73d6bfc3f56dcc5dc716b57c
ARP-Admin-Dev <Gertus22@gmail.com>
2025-02-06 15:38:57 +0100

    Update webpages and info

---
+++ assets/js/scripts.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function() {
+    
+    var parentDomain = window.location.hostname;
+
+    // var iframe = document.getElementById("twitch-iframe");
+    // var videoId = "2364316732";
+    // var startTime = "02h07m38s";
+    // iframe.src = `https://player.twitch.tv/?video=${videoId}&t=${startTime}&parent=${parentDomain}&muted=true`;
+
+
+    var iframe1 = document.getElementById("twitch-iframe1");
+    var channel1 = "awesomeprice"; // Twitch-gebruikersnaam van de eerste streamer
+    iframe1.src = `https://player.twitch.tv/?channel=${channel1}&parent=${parentDomain}&muted=true`;
+
+    var iframe2 = document.getElementById("twitch-iframe2");
+    var channel2 = "kann3j"; // Twitch-gebruikersnaam van de tweede streamer
+    iframe2.src = `https://player.twitch.tv/?channel=${channel2}&parent=${parentDomain}&muted=true`;
+});
+
